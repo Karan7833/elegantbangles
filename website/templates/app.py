a024d07f605f60a6e039fe9fe0bfb0eaf35c5767
Karan7833 <karanrathour1811@gmail.com>
2025-09-12 19:09:22 +0530

    chore: initialize repo and add project files

---
+++ website/templates/app.py
@@ -0,0 +1,20 @@
+# from flask import Flask, render_template
+# from flask_sqlalchemy import SQLAlchemy 
+# from flask import request, jsonify
+
+
+
+# app = Flask(__name__)
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+# # @app.route('/karanratour1811@gmail.com', methods=['POST'])
+# # def endpoint():
+# #     data = request.get_json()
+# #     if not data:
+# #         return jsonify({"error": "Invalid JSON"}), 400
+# #     # process data
+# #     return jsonify({"message": "Success"})
+
+# if __name__ == "__main__":
+#     app.run(debug=True, port=8000)
